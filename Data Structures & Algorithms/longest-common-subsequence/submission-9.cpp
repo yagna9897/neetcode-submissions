2d220329fe9d29fcfd46c38271d911efd245052f
yagna9897 <76590976+yagna9897@users.noreply.github.com>
2026-09-24 08:47:07 +0530

    Add: longest-common-subsequence - submission-9

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-9.cpp
@@ -0,0 +1,30 @@
+class Solution {
+    int Solve(const string& text1, const string& text2)
+{
+    int n1 = text1.length();
+    int n2 = text2.length();
+
+    std::vector < std::vector<int>> dp(n1+1, std::vector<int>(n2+1, 0));
+    
+    for (int i = n1 - 1; i >= 0; i--)
+    {
+        for (int j = n2 - 1; j >= 0; j--)
+        {
+            if (text1[i] == text2[j])
+            {
+                dp[i][j] = 1 + dp[i + 1][j + 1];
+            }
+            else
+            {
+                dp[i][j] = std::max(dp[i + 1][j], dp[i][j + 1]);
+            }
+        }
+    }
+    return dp[0][0];
+}
+
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        return Solve(text1, text2);
+    }
+};

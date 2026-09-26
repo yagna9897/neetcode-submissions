7f91e60f9a3a1bc7cdac99b01f4396e6f32a5371
yagna9897 <76590976+yagna9897@users.noreply.github.com>
2026-09-26 06:55:40 +0530

    Add: rotate-matrix - submission-0

---
+++ Data Structures & Algorithms/rotate-matrix/submission-0.cpp
@@ -0,0 +1,32 @@
+class Solution {
+    void ReverseVector(vector<int>& vec)
+    {
+        int n = vec.size();
+        for(int i = 0; i <= (n-1)/2; i++ )
+        {
+            std::swap(vec[i], vec[n-i-1]);
+            /*int temp = vec[n-i-1];
+            vec[n-i-1] = vec[i];
+            vec[i] = temp;*/
+        }
+    }
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        
+        int n = matrix.size();
+        int m = matrix[0].size();
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < i ; j++)
+            {
+                std::swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+        for(int i = 0; i < n; i++)
+        {
+            std::vector<int> vec = matrix[i];
+            ReverseVector(vec);
+            matrix[i] = vec;
+        }
+    }
+};
